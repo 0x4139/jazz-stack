86e752af2507b691e1b1e20fb75ab58691f66d93
Vali Malinoiu <0x4139@gmail.com>
2022-04-29 23:03:37 +0300

    feat(jazz-stack): :art: add indexpage, add daisy

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,32 +1,19 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="hero min-h-screen" style={{ backgroundImage: `url(https://media.timeout.com/images/105617426/750/422/image.jpg)` }}>
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-secondary-content">
+        <div className="max-w-3xl">
+          <h1 className="mb-5 text-8xl font-bold">JAZZ STACK</h1>
+          <p className="mb-5">Check the README.MD file for instructions on how to get this project deployed</p>
+          <div className="w-full flex justify-center">
+            <button className="w-32 btn btn-active btn-primary">Sign Up</button>
+            <div className="divider divider-horizontal">OR</div>
+            <button className="btn w-32 btn-active btn-primary">Login</button>
+          </div>
+          <img src={require("./../../assets/images/remix.svg")} alt="" />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
